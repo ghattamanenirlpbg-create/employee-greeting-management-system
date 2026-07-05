d9a5a0dc20e67cffe58a7e13a7fe25792623f319
RL Prasad Babu Ghattamaneni <ghattamaneni.rlpbg@gmail.com>
2026-07-05 18:33:24 +0530

    Configured Vercel API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: API,
 });
 
 export default api;
